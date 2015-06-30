192343c71f3deb300fca71b37e4b79a027e56277
RosmaryFC <Rosmaryfc@hotmail.com>
2015-06-30 18:23:16 -0400

    creating todo card, commiting to pull from card-maps

---
+++ NowFeed/app/src/main/java/com/jaellysbales/nowfeed/TodoCard.java
@@ -0,0 +1,17 @@
+package com.jaellysbales.nowfeed;
+
+import android.view.View;
+
+/**
+ * Created by c4q-rosmary on 6/30/15.
+ */
+public class TodoCard {
+
+    View todoView;
+
+    public TodoCard (View todoView){
+        this.todoView = todoView;
+    }
+
+    git st
+}
